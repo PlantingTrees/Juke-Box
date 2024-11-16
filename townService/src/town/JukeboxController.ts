a764dcfa89cd22f788d2a26857969ac93d445a98
Nnamdi Okeke <plantingTrees@gmail.com>
2024-11-16 16:37:22 -0500

    CI

--- townService/src/town/JukeboxController.ts
+++ townService/src/town/JukeboxController.ts
@@ -13,7 +13,10 @@ export default class JukeboxController {
   }
 
   // eslint-disable-next-line class-methods-use-this
-  public adjustVolume(volume: number): void {}
+  public adjustVolume(volume: number): void {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    volume = 1.0;
+  }
 
   addToQueue(song: Song): void {
     this.localQueue.push(song);
