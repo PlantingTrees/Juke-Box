45fc9a0bdd65f61011b8af12fce59b9f6753f989
Nnamdi Okeke <plantingTrees@gmail.com>
2024-11-19 20:52:38 -0500

    brnch not merging check??

--- frontend/src/components/Town/interactables/Jukebox.tsx
+++ frontend/src/components/Town/interactables/Jukebox.tsx
@@ -82,7 +82,6 @@ export default function JukeboxArea(): JSX.Element {
             closeModal();
             coveyTownController.unPause();
           }}
-          
           size='2xl'>
           <ModalOverlay />
           <ModalContent
